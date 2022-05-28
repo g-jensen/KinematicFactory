0e5e5e192954aa859e09fde996cc52dff86abce3
Greg Jensen <77466937+g-jensen@users.noreply.github.com>
2022-05-28 00:16:10 -0400

    Cleanup

--- KinematicFactory/Game.h
+++ KinematicFactory/Game.h
@@ -2,9 +2,23 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <math.h>
+#include <iostream>
+#include <array>
+
+#include "imgui/imgui.h"
+#include "imgui/imgui-SFML.h"
+
+#include "Limb.h"
+#include "Arm.h"
+#include "ArmItem.h"
+#include "ImGuiHelper.h"
+
 class Game
 {
 public:
+	Game();
+	void run();
 	static sf::RenderWindow* window;
 	static sf::Vector2f mouse_pos;
 };
